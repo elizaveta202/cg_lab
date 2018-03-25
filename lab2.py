468a2df27a53c8421d90a33b2fb721d08e700891
L L <elizaveta202@yandex.ru>
2018-03-26 01:11:19 +0300

    Change main circle radiu from GUI feature added

--- lab2.py
+++ lab2.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
+from matplotlib.widgets import TextBox
 
 fig = plt.figure()
 fig.set_dpi(100)
@@ -12,11 +13,12 @@ rm = 1
 ax = plt.axes(xlim=(-Rb-1, Rb+1), ylim=(-Rb-1, Rb+1))
 patch = plt.Circle((0, 0), rm, fill=False, fc='y')
 main_circle = plt.Circle((0, 0), Rb, fill=False, fc='y')
+ax.add_patch(main_circle)
+
 
 def init():
     patch.center = (0, 0)
     ax.add_patch(patch)
-    ax.add_patch(main_circle)
     return patch,
 
 def animate(i):
@@ -26,12 +28,36 @@ def animate(i):
     patch.center = (x, y)
     return patch,
 
-anim = animation.FuncAnimation(fig, animate,
+def animate2(i, Rb, rm):
+    x, y = patch.center
+    x = (Rb-rm) * np.sin(np.radians(i))
+    y = (Rb-rm) * np.cos(np.radians(i))
+    patch.center = (x, y)
+    return patch,
+
+anim = animation.FuncAnimation(fig, animate2,
+                               fargs=(Rb, rm),
                                init_func=init,
                                frames=360,
                                interval=1,
-                               blit=True)
+                               blit=False)
 
-# plt.xlim(Rb+1, Rb+1)
-# plt.ylim(Rb+1, Rb+1)
+def submit(text):
+    Rb = int(text)
+    print(Rb)
+    main_circle.set_radius(Rb)
+    plt.draw()
+
+    anim.__init__(fig, animate2, fargs=(Rb, rm),
+                            init_func=init,
+                            frames=360,
+                            interval=1,
+                            blit=False)
+    anim.frame_seq = anim.new_frame_seq()
+    anim.event_source.start()
+
+axbox = plt.axes([0.1, 0.05, 0.08, 0.08])
+text_box = TextBox(axbox, 'R')
+text_box.on_submit(submit)
+
 plt.show()
